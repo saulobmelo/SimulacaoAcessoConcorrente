cc65ed85964d4a0eeb23cd7306136407bfa709e4
SaulinhoDevs <165098314+saulobmelo@users.noreply.github.com>
2025-08-28 19:01:03 -0300

    Add interface de linha de comando para servidor e cliente

--- program/src/cliente/NoCliente.java
+++ program/src/cliente/NoCliente.java
@@ -2,17 +2,25 @@ package cliente;
 
 import comum.Mensagem;
 import comum.RecursoCompartilhado;
+
 import java.io.*;
 import java.net.*;
 import java.util.Random;
 
 public class NoCliente {
-    private static final String HOST = "localhost";
-    private static final int PORTA = 5000;
+    private static String HOST = "localhost";
+    private static int PORTA = 5000;
+    private static String nomeDoNo = "Cliente";
+
     private static int relogio = 0;
     private static RecursoCompartilhado recurso = new RecursoCompartilhado();
 
     public static void main(String[] args) throws Exception {
+        // args[0] = host, args[1] = porta, args[2] = nomeDoNo
+        if (args.length >= 1) HOST = args[0];
+        if (args.length >= 2) PORTA = Integer.parseInt(args[1]);
+        if (args.length >= 3) nomeDoNo = args[2];
+
         Random random = new Random();
         while (true) {
             Thread.sleep(2000 + random.nextInt(3000));
@@ -26,7 +34,7 @@ public class NoCliente {
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream());
              ObjectInputStream in = new ObjectInputStream(socket.getInputStream())) {
 
-            out.writeObject(new Mensagem("REQUISICAO", "Quero acessar o recurso", relogio));
+            out.writeObject(new Mensagem("REQUISICAO", nomeDoNo + " quer acessar o recurso", relogio));
             Mensagem resposta = (Mensagem) in.readObject();
             if (resposta.getTipo().equals("PERMISSAO")) {
                 acessarRecursoCritico();
@@ -37,17 +45,17 @@ public class NoCliente {
 
     private static void acessarRecursoCritico() throws Exception {
         relogio++;
-        System.out.println("[Cliente] Acessando recurso crítico... Relógio=" + relogio);
+        System.out.println("[" + nomeDoNo + "] Acessando recurso crítico... Relógio=" + relogio);
         recurso.incrementar();
-        System.out.println("[Cliente] Valor atual do contador = " + recurso.getValor());
-        recurso.salvarCheckpoint("checkpoint_cliente.dat");
+        System.out.println("[" + nomeDoNo + "] Valor atual do contador = " + recurso.getValor());
+        recurso.salvarCheckpoint("checkpoint_" + nomeDoNo + ".dat");
     }
 
     private static void liberarAcesso() throws Exception {
         relogio++;
         try (Socket socket = new Socket(HOST, PORTA);
              ObjectOutputStream out = new ObjectOutputStream(socket.getOutputStream())) {
-            out.writeObject(new Mensagem("LIBERACAO", "Liberei o recurso", relogio));
+            out.writeObject(new Mensagem("LIBERACAO", nomeDoNo + " liberou o recurso", relogio));
         }
     }
 }
